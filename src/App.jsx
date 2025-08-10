4fe4975777658d4320f2e39cb7444bc144ad9eed
RamirezAgus <rlagustin0@gmail.com>
2025-08-10 15:20:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from '@components/Navbar'
+
+const App = () => {
+  return (
+    <div className="font-montserrat">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav>
+            Nav Bar
+        </nav>
+    )
+}
+
+export default Navbar;
