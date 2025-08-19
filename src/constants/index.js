88fa83e1d2c9c14ba290e5851282a412dd236627
RamirezAgus <rlagustin0@gmail.com>
2025-08-19 11:42:13 -0300

    styling travel section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import { img } from "framer-motion/client"
+
 export const exploreTravels = [
     {
         id: 'travel-1',
@@ -56,16 +58,143 @@ export const exploreTravels = [
         id: 'travel-2',
         imgUrl: '/src/assets/caribe1.webp',
         title: 'Caribe',
+        packages: [
+            {
+                title: "Paquete 1",
+                subtitle: "Bayahibe",
+                imgUrl: "/src/assets/travelsimg/bayahibe/bayahibe.webp",
+                description: `INCLUYE:
+                -VUELO IDA Y VUELTA CON CUPO CONFIRMADO DE ARAJET
+                -HOTEL 5 * ALL INCLUSIVE
+                -TODOS LOS TRASLADOS
+                ITINERARIO , 7 NOCHES
+                CONOCE EL PARAÍSO EN SU MÁXIMO ESPLENDOR Y DISFRUTA DE UN HERMOSO HOTEL 5***** ALL INCLUSIVE
+                1599 USD POR PERSONA
+                `
+            },
+            {
+                title: "Paquete 2",
+                subtitle: "Cancun",
+                imgUrl: "/src/assets/travelsimg/cancun/cancun.webp",
+                description: `INCLUYE:
+                -VUELOS IDA Y VUELTA 
+                -HOTEL 5 * ALL INCLUSIVE
+                -TODOS LOS TRASLADOS
+                ITINERARIO :8 NOCHES,  9 DÍAS
+                CONOCE LAS HERMOSAS PLAYAS DE CANCÚN , 
+                Y DISFRUTA DE UN ALOJAMIENTO 5 
+                ***** ALL INCLUSIVE.
+                1799 USD POR PERSONA`
+            },
+            {
+                title: "Paquete 3",
+                subtitle: "Aruba",
+                imgUrl: "/src/assets/travelsimg/aruba/aruba.webp",
+                description: `INCLUYE:
+                -AÉREO IDA Y VUELTA  CON EQUIPAJE 
+                -HOTELERIA ALL INCLUSIVE
+                -TODOS LOS TRASLADOS IN / OUT Y AÉREO/TERRESTRE
+                -CONSULTAR POR ACTIVIDADES Y EXCURSIONES.
+                ITINERARIO:  7 NOCHES , 8 DÍAS 
+                VISITA LAS MEJORES PLAYAS DE ARUBA EN UN PAQUETE TODO RESUELTO.
+                DESDE 2699 USD POR PERSONA`
+            }
+        ]
     },
     {
         id: 'travel-3',
         imgUrl: '/src/assets/exoticos1.webp',
         title: 'Exoticos',
+        packages: [
+            {
+                title: "Paquete 1",
+                subtitle: "Emiratos Arabes Unidos",
+                imgUrl: "/src/assets/travelsimg/emiratos/emiratos.webp",
+                description: `-AÉREO IDA Y VUELTA  CON EQUIPAJE 
+                -HOTELERIA
+                -TODOS LOS TRASLADOS IN / OUT Y AÉREO/TERRESTRE
+                - ACTIVIDADES Y EXCURSIONES.
+                ITINERARIO , 6 NOCHES 
+                VISITANDO: DUBAI , ABU DHABI , DOHA.
+                PRECIOS VARIABLES SEGÚN FECHA Y DISPONIBILIDAD. 
+                ¡CONSULTANOS PARA OBTENER LA MEJOR TARIFA PERSONALIZADA!`
+            },
+            {
+                title: "Paquete 2",
+                subtitle: "Sudeste Asiatico",
+                imgUrl: "/src/assets/travelsimg/sudeste/sudeste.webp",
+                description: `-AÉREO IDA Y VUELTA  CON EQUIPAJE 
+                -HOTELERIA
+                -TODOS LOS TRASLADOS IN / OUT Y AÉREO/TERRESTRE
+                - ACTIVIDADES Y EXCURSIONES
+                ITINERARIO 12 NOCHES 13 DIAS
+                VISITANDO : KUALA LUMPUR , MALACCA , SINGAPUR , BANGKOK , PHUKET.
+                PRECIOS VARIABLES SEGÚN FECHA Y DISPONIBILIDAD.
+                ¡CONSULTANOS PARA OBTENER LA MEJOR TARIFA PERSONALIZADA!`
+            },
+            {
+                title: "Paquete 3",
+                subtitle: "Turquia",
+                imgUrl: "/src/assets/travelsimg/turquia/turquia.webp",
+                description: `-AÉREO IDA Y VUELTA  CON EQUIPAJE 
+                -HOTELERIA
+                -TODOS LOS TRASLADOS IN / OUT Y AÉREO/TERRESTRE
+                - ACTIVIDADES Y EXCURSIONES
+                ITINERARIO 12 NOCHES 13 DÍAS
+                VISITANDO : ESTAMBUL , CAPADOCIA , PAMUKKALE , KUSADASI , DUBAI.`
+            }
+        ]
     },
     {
         id: 'travel-4',
         imgUrl: '/src/assets/brasil1.webp',
         title: 'Brasil',
+        packages: [
+            {
+                title: "Paquete 1",
+                subtitle: "Rio de Janeiro",
+                imgUrl: "/src/assets/travelsimg/rio/rio.webp",
+                description: `-AÉREO IDA Y VUELTA  CON EQUIPAJE 
+                -HOTELERIA
+                -TODOS LOS TRASLADOS IN / OUT Y AÉREO/TERRESTRE
+                ITINERARIO 7 NOCHES , 8 DIAS 
+                DISFRUTA DE LAS HERMOSAS PLAYAS DE RIO DE JANEIRO.
+                DESDE 699 USD POR PERSONA`
+            },
+            {
+                title: "Paquete 2",
+                subtitle: "Buzios",
+                imgUrl: "/src/assets/travelsimg/buzios/buzios.webp",
+                description: `-AÉREO IDA Y VUELTA  CON EQUIPAJE 
+                -HOTELERIA
+                -TODOS LOS TRASLADOS IN / OUT Y AÉREO/TERRESTRE
+                ITINERARIO 7 NOCHES , 8 DIAS 
+                CONOCE LAS HERMOSAS PLAYAS DE RIO DE JANEIRO Y BUZIOS.
+                DESDE 749 USD POR PERSONA`
+            },
+            {
+                title: "Paquete 3",
+                subtitle: "Fortaleza",
+                imgUrl: "/src/assets/travelsimg/fortaleza/fortaleza.webp",
+                description: `-AÉREO IDA Y VUELTA  CON EQUIPAJE 
+                -HOTELERIA 4 *
+                -TODOS LOS TRASLADOS IN / OUT Y AÉREO/TERRESTRE
+                ITINERARIO 7 NOCHES  , 8 DIAS 
+                DISFRUTA DE LAS RELAJANTES PLAYAS DE FORTALEZA
+                DESDE 829 USD POR PERSONA`
+            },
+            {
+                title: "Paquete 4",
+                subtitle: "Florianopolis",
+                imgUrl: "/src/assets/travelsimg/florianopolis/florianopolis.webp",
+                description: `-AÉREO IDA Y VUELTA  CON EQUIPAJE 
+                -HOTELERIA
+                -TODOS LOS TRASLADOS IN / OUT Y AÉREO/TERRESTRE
+                ITINERARIO 7 NOCHES 8 DIAS .
+                AVENTURATE EN ESTE INCREIBLE DESTINO Y DISFRUTA DE SUS PLAYAS.
+                DESDE 499 USD POR PERSONA`
+            }
+        ]
     },
     {
         id: 'travel-5',
